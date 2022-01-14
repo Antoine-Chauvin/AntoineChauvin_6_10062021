b5be83a7905ba4e426bac134639041278eec7dfd
Antoine Chauvin <antoine.chavin61@outlook.fr>
2022-01-15 00:23:43 +0100

    Verifie_good_pour_prese

--- back_end/models/sauces.js
+++ back_end/models/sauces.js
@@ -8,12 +8,20 @@ const sauceSchema = mongoose.Schema({
     mainPeppeer: {type: String, require: true},
     imageUrl: {type: String, require: true},
     heat: {type: Number, require: true},
-    likes: {type: Number, default: 0},
-    dislikes:{type: Number, default:0},
+   /*  likes: {type: Number, default: 0},
+    dislikes:{type: Number, default:0}, */
     usersLiked: [String],
     usersDisliked: [String] ,
-})
+},
+{toJSON: {virtuals: true}}
+)
 
+sauceSchema.virtual("likes").get(function(){
+    return this.usersLiked.length
+});
+sauceSchema.virtual("dislikes").get(function(){
+    return this.usersDisliked.length
+});
 
 
 module.exports = mongoose.model('Sauce', sauceSchema)
